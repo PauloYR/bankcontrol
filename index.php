264877492f1e51648c7c427e0535360995c901de
Paulo Iury <pauloyr@outlook.com>
2018-05-20 22:11:39 -0300

    Update index.php
    porque eu quero

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ require_once("controle/html/head.php");
 	    <li data-target="#carroseelIndex" data-slide-to="0" class="active"></li>
 	    <li data-target="#carroseelIndex" data-slide-to="1"></li>
 	    <li data-target="#carroseelIndex" data-slide-to="2"></li>
-	    <li data-target="#carroseelIndex" data-slide-to="3"></li>
+	    <li data-target="#carroseelIndex" data-slide-to="4"></li>
 	  </ol>
 	  <div class="carousel-inner ">
 	    <div class="carousel-item active">
